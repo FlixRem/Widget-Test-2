0638dcf813884e450786e12c2121bad08f0d99f7
FlixRem <felixsneuerlaptop@gmail.com>
2025-03-17 14:33:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
-import "./widgets/floating_anki_widget"; // Floating Widget importieren
+import React from "react";
+import FloatingAnkiWidget from "./widgets/floating_anki_widget"; // Floating Widget importieren
+
+function App() {
+  console.log("📌 Plugin gestartet!"); // Debugging: Zeigt an, dass das Plugin läuft
+
+  return (
+    <div>
+      <h1>RemNote Anki Dashboard</h1>
+      <FloatingAnkiWidget /> {/* Hier wird das Widget jetzt wirklich gerendert */}
+    </div>
+  );
+}
+
+export default App;
